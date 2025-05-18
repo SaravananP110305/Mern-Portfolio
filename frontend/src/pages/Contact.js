d912f472e647c96f8f663f56b221eea8965aa0b7
Saravanan P <parathk285@gmail.com>
2025-05-18 11:17:26 +0530

    Update Contact.js

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -15,7 +15,7 @@ function Contact() {
   const handleSubmit = async (e) => {
   e.preventDefault();
   try {
-    const response = await fetch('http://localhost:5000/api/contact', {
+    const response = await fetch('https://mern-portfolio-backend-8t9q.onrender.com/api/contact', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
